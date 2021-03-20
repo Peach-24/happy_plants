85f4ecc509673006024362cf20a959699f9fe74e
Josh Peachey <joshpeachey24@gmail.com>
2021-03-20 09:03:43 +0000

    Adds component folders, files + placeholder code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Happy Plants</h1>
-      <p>Weather tool for your garden</p>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Happy Plants</h1>
+        <p>Weather tool for your garden</p>
+      </div>
+    );
+  }
 }
-
-export default App;
