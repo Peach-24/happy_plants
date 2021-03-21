349185808a667b1be2f270e80be9332ea78305eb
Josh Peachey <joshpeachey24@gmail.com>
2021-03-21 12:23:45 +0000

    Has one single file for front-end cypress tests in /cypress/integration/App.spec.js

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -30,3 +30,16 @@ describe("The form component", () => {
     cy.get("#needs-sunshine");
   });
 });
+
+describe("The Happiness component", () => {
+  it("has an icon for the overall happiness", () => {
+    cy.visit("localhost:3000");
+    cy.get("#emotion-icon");
+  });
+  it("has a text description of happiness level'", () => {
+    cy.get("#emotion-text");
+  });
+  it("has a text message for guidance / relating to weather'", () => {
+    cy.get("#emotion-text");
+  });
+});
